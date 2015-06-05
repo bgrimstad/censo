86311d93a8c035e779019d02c43928fdb18c47a8
Bjarne Grimstad <bjarne.grimstad@gmail.com>
2015-06-05 10:02:00 +0200

    Added new constraint class for general approximants from SPLINTER

---
+++ OptimizationProblem/constraintapproximant.cpp
@@ -0,0 +1,5 @@
+#include "constraintapproximant.h"
+
+ConstraintApproximant::ConstraintApproximant()
+{
+}
---
+++ OptimizationProblem/constraintapproximant.h
@@ -0,0 +1,10 @@
+#ifndef CONSTRAINTAPPROXIMANT_H
+#define CONSTRAINTAPPROXIMANT_H
+
+class ConstraintApproximant
+{
+public:
+    ConstraintApproximant();
+};
+
+#endif // CONSTRAINTAPPROXIMANT_H
